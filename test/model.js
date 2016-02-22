d1c6e6410c0616083ee33c7b2a3181811a2f0895
Tomoya Kishimoto <h17184b@gmail.com>
2016-02-23 08:39:25 +0900

    first commit

---
+++ test/model.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const model = require('../helper.js');
+const parser = require('../peg.js');
+
+describe('model test', () => {
+	it('', () => {
+    let target = new model.Target();
+	});
+});
