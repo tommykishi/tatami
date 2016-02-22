65d95c1cac0bad6abc711d037c969889f11ca7e8
Tomoya Kishimoto <h17184b@gmail.com>
2016-02-23 08:39:25 +0900

    first commit

---
+++ model.js
@@ -0,0 +1,56 @@
+'use strict'
+var util = {};
+const _ = require('lodash');
+
+util.TreeNode = function(object) {
+	this.type = object.type;
+	this.name = object.name || [];
+	this.ws = object.whitespace || [];
+	this.ex = object.extension || [];
+	this.depth = object.depth || 0;
+	this.rel = new Map();
+};
+
+util.Tree = function() {
+	this.counter = 0;
+	var arr = [];
+	this.add = function(args) {
+		_.each(args, function(object) {
+			var node = new util.TreeNode(object);
+			arr.push(node);
+		});
+		this.scan();
+	};
+	this.getNode = function() {
+		return arr;
+	};
+    this.getPath = function() {
+        _.map(arr, function(node) {
+            if(node.rel.get('parent')){
+                console.log(`${__dirname}/${node.rel.get('parent').name.join("")}/${node.name.join("")}.${node.ex.join("")}`);
+            }else{
+                console.log(`${__dirname}/${node.name.join("")}`);
+            }
+        });
+    };
+	this.scan = function() {
+		for (var i = 0; i < arr.length; i++) {
+			for (var j = 1; j < arr.length; j++) {
+				if (arr.length == i + j) break;
+				if (arr[i].depth < arr[i + j].depth) {
+					arr[i + j].rel.set("parent", arr[i]);
+					break;
+				} else if (arr[i].depth == arr[i + j].depth) {
+					arr[i + j].rel.set("parent", arr[i].rel.get("parent"));
+					break;
+				} else {
+					//arr[i].depth > arr[i+j].depth
+					arr[i].rel.set("parent", arr[i - 1].rel.get("parent"));
+					break;
+				}
+			}
+		}
+	};
+};
+
+module.exports = util;
---
+++ test/parser.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const parser = require('../peg.js');
+const should = require('chai').should();
+
+describe('myPegjs parser test', () => {
+	describe('if file be parsed corrctly ', () => {
+		it('general file', () => {
+			parser.parse('apple.md').should.have.property('type', 'file');
+		});
+		it('file name including number', () => {
+			parser.parse('111.md').should.have.property('type', 'file');
+		});
+		it('file name including non alphabets', () => {
+			//parser.parse('りんご.md').should.have.property('type', 'file');
+		});
+		it('file name including uppercase letter', () => {
+			parser.parse('Apple.md').should.have.property('type', 'file');
+		});
+		it('file without extension', () => {
+    	parser.parse('Apple').should.have.property('type', 'file');
+		});
+	});
+	describe('if directory be parsed correctly', () => {
+		it('general dir', () => {
+			parser.parse('apple/').should.have.property('type', 'directory');
+		});
+		it('dir including number', () => {
+			parser.parse('111/').should.have.property('type', 'directory');
+		});
+		it('dir including non alphabets', () => {
+			//parser.parse('りんご/').should.have.property('type', 'directory');
+		});
+		it('dir including uppercase letter', () => {
+			parser.parse('Apple/').should.have.property('type', 'directory');
+		});
+	});
+	describe('if comment be parsed correctly', () => {
+		it('single comment', () => {
+			parser.parse('//singlecomment').should.have.property('type', 'comment');
+		});
+		it('multiline comment', () => {
+			parser.parse('/*multilinecomment*/').should.have.property('type', 'comment');
+		});
+		it('comment including number', () => {
+			parser.parse('//single111').should.have.property('type', 'comment');
+			parser.parse('/*multiline111*/').should.have.property('type', 'comment');
+		});
+		it('comment including uppercase letter', () => {
+			parser.parse('//SingleComment').should.have.property('type', 'comment');
+			parser.parse('/*MultilineComment*/').should.have.property('type', 'comment');
+		});
+		it('comment including white space', () => {
+			parser.parse('//single  comment').should.have.property('type', 'comment');
+			parser.parse('/*multiline  comment*/').should.have.property('type', 'comment');
+		});
+		it('comment including non alphabets', () => {
+			//parser.parse('//コメント').should.have.property('type', 'comment');
+			//parser.parse('/*コメント*/').should.have.property('type', 'comment');
+		});
+	});
+	describe('common test', () => {
+		it('incorrect input', () => {
+			(function(){parser.parse('app le.md')}).should.throw();
+			(function(){parser.parse('apple.m d')}).should.throw();
+			(function(){parser.parse('app/le.md')}).should.throw();
+			(function(){parser.parse('app?le.md')}).should.throw();
+			(function(){parser.parse('app*le.md')}).should.throw();
+			(function(){parser.parse('app:le.md')}).should.throw();
+			(function(){parser.parse('app|le.md')}).should.throw();
+			(function(){parser.parse('app<le.md')}).should.throw();
+			(function(){parser.parse('app>le.md')}).should.throw();
+		});
+	});
+});
