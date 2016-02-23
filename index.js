966d4dd68e30e002e9af89684943681b1c7af1fe
Tomoya Kishimoto <h17184b@gmail.com>
2016-02-23 11:16:13 +0900

    fix test

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const fs = require('fs'),
-	  readline = require('readline'),
-	  rs = fs.ReadStream('./Tatamifile'),
-	  rl = readline.createInterface({
-	    'input': rs,
-			'output': {}
-	  });
-
-const parser = require('./peg.js');
-const model = require('./model.js');
+  readline = require('readline'),
+  rs = fs.ReadStream('./Tatamifile'),
+  rl = readline.createInterface({
+    'input': rs,
+    'output': {}
+  }),
+  parser = require('./peg.js'),
+  model = require('./model.js');
 
 const tree = new model.Tree();
 const arr = [];
 
 rl.on('line', function(line) {
-	arr.push(parser.parse(line));
+  arr.push(parser.parse(line));
 });
 
 rl.on('close', function() {
-	var tree = new model.Tree();
-	tree.add(arr);
-	tree.getPath();
+  var tree = new model.Tree();
+  tree.add(arr);
+  tree.getPath();
 });
