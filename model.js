368677fc587f2b5adba9b5da61cda9f3361ee6a9
Tomoya Kishimoto <h17184b@gmail.com>
2016-03-12 12:43:05 +0900

    refactor

--- model.js
+++ model.js
@@ -4,89 +4,120 @@ var util = {};
 const _ = require('lodash');
 
 util.TreeNode = function(object) {
-  this.type = object.type;
-  this.name = object.name || [];
-  this.depth = object.depth || 0;
-  this.rel = new Map();
+	this.type = object.type;
+	this.name = object.name || [];
+	this.depth = object.depth || 0;
+	this.rel = new Map();
 };
 
 util.Tree = function(nodearr) {
-  const arr = [];
+	const arr = [];
 
-  this.getArray = function() {
-    return arr;
-  };
+	this.getArray = function() {
+		return arr;
+	};
 
-  this.getPath = function() {
-    const patharr = [];
-    _.map(arr, function(node) {
-      patharr.push(node.path);
-    });
-    return patharr;
-  };
+	this.getPath = function() {
+		const patharr = [];
+		_.map(arr, function(node) {
+			patharr.push(node.path);
+		});
+		return patharr;
+	};
 
-  this.plant = function(args) {
-    _.each(args, function(object) {
-      let node = new util.TreeNode(object);
-      if (node.type != 'comment') {
-        arr.push(node);
-      }
-    });
-    this.createAst(arr.length - 1);
-    return this;
-  };
+	this.plant = function(args) {
+		_.each(args, function(object) {
+			let node = new util.TreeNode(object);
+			if (node.type != 'comment') {
+				arr.push(node);
+			}
+		});
+		this.createAst(arr.length - 1);
+		return this;
+	};
 
-  this.createAst = function(c) {
-    if (c < 0) {
-      return this;
-    } else if (arr[c].depth == 0) {
-      this.createAst(c - 1);
-    } else {
-      let i = 1;
-      while (1) {
-        if ((c - i) < 0) break;
-        if (arr[c].depth > arr[c - i].depth) break;
-        i++;
-      }
-      arr[c].rel.set("parent", arr[c - i]);
-      this.createAst(c - 1);
-    }
-  };
+	this.createAst = function(c) {
+		if (c < 0) {
+			return this;
+		} else if (arr[c].depth == 0) {
+			this.createAst(c - 1);
+		} else {
+			let i = 1;
+			while (1) {
+				if ((c - i) < 0) break;
+				if (arr[c].depth > arr[c - i].depth) break;
+				i++;
+			}
+			arr[c].rel.set("parent", arr[c - i]);
+			this.createAst(c - 1);
+		}
+	};
 
-  this.pather = function(c) {
-    if (c < 0) {
-      return;
-    } else {
-      const path = [];
-      let t = c;
-      while (1) {
-        let parent = arr[t].rel.get('parent');
-        if (!parent) break;
-        path.unshift(parent.name);
-        t -= t - _.indexOf(arr, parent);
-      }
-      this.pathname(arr[c], path);
-      this.pather(c - 1);
-    }
-  };
+	this.pather = function(c) {
+		if (c < 0) {
+			return;
+		} else {
+			const path = [];
+			let t = c;
+			while (1) {
+				let parent = arr[t].rel.get('parent');
+				if (!parent) break;
+				path.unshift(parent.name);
+				t -= t - _.indexOf(arr, parent);
+			}
+			this.pathname(arr[c], path);
+			this.pather(c - 1);
+		}
+	};
 
-  this.pathname = function(node, path) {
-    if (node.type == 'file') {
-      if (path.length != 0) {
-        node.path = `/${path.join("/")}/${node.name}`
-      } else {
-        node.path = `/${node.name}`
-      }
-    } else {
-      if (path.length != 0) {
-        node.path = `/${path.join("/")}/${node.name}/`
-      } else {
-        node.path = `/${node.name}/`
-      }
-    }
-  };
+	this.pathname = function(node, path) {
+		if (node.type == 'file') {
+			if (path.length != 0) {
+				node.path = `/${path.join("/")}/${node.name}`
+			} else {
+				node.path = `/${node.name}`
+			}
+		} else {
+			if (path.length != 0) {
+				node.path = `/${path.join("/")}/${node.name}/`
+			} else {
+				node.path = `/${node.name}/`
+			}
+		}
+	};
 
-  this.plant(nodearr).pather(arr.length - 1);
+	this.plant(nodearr).pather(arr.length - 1);
+};
+
+util.argValidate = function(arg) {
+	if (!arg.length) {
+		return {
+			message: "Good",
+			status: true
+		}
+	} else {
+		return {
+			message: "Too many args",
+			status: false
+		}
+	}
+};
+
+util.message = function(treeArr){
+	const group = _.groupBy(treeArr,'type');
+	if (1 < group.directory.length) {
+		if (1 < group.file.length) {
+			return `${group.directory.length} directories, ${group.file.length} files`;
+		}else{
+			return `${group.directory.length} directories, ${group.file.length} file`;
+		}
+	} else {
+		if (1 < group.file.length) {
+			return `${group.directory.length} directory, ${group.file.length} files`;
+		}else{
+			return `${group.directory.length} directory, ${group.file.length} file`;
+		}
+	}
 };
 
 module.exports = util;
--- tatami-rollback.js
+++ tatami-rollback.js
@@ -1,15 +1,22 @@
+#!/usr/bin/env node
 //tatami rollback
+
 'use strict'
 const program = require('commander'),
 	_ = require('lodash'),
 	fs = require('fs'),
-	colors = require('colors'),
-	async = require('async');
-
-const parser = require('./peg/peg.js'),
+	async = require('async'),
+	 parser = require('./peg/peg.js'),
 	model = require('./model.js');
 
 program.parse(process.argv);
+
+const flag = model.argValidate(program.args);
+
+if (!flag.status) {
+	console.log(flag.message);
+	process.exit(1);
+}
 
 fs.stat('./Tatamifile', (err, stat) => {
 	const arr = [];
@@ -31,7 +38,7 @@ fs.stat('./Tatamifile', (err, stat) => {
 		});
 
 	} else if (err.code == 'ENOENT') {
-		console.log(colors.red.bold('Tatamifile not found'));
+		console.log('Tatamifile not found');
 	} else {
 		console.log('Some other error: ', err.code);
 	}
@@ -58,24 +65,6 @@ const createTree = function(arr) {
 		}
 	], function(err, result) {
 		if (err) console.error(err);
-		console.log(colors.red.bold('tatami rollback'));
-		message(treeArr);
+		console.log(`${model.message(treeArr)}`);
 	});
-};
-
-const message = function(treeArr){
-	const group = _.groupBy(treeArr,'type');
-	if (1 < group.directory.length) {
-		if (1 < group.file.length) {
-			console.log(`${group.directory.length} directories, ${group.file.length} files`);
-		}else{
-			console.log(`${group.directory.length} directories, ${group.file.length} file`);
-		}
-	} else {
-		if (1 < group.file.length) {
-			console.log(`${group.directory.length} directory, ${group.file.length} files`);
-		}else{
-			console.log(`${group.directory.length} directory, ${group.file.length} file`);
-		}
-	}
 };
